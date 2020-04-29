a5013c9b39fe388491b70508ed10f56058800dd9
Olia1011 <olgamelnyk10111989@gmail.com>
2020-04-29 14:43:38 +0300

    Updates

---
+++ precache-manifest.4d7b39664349c16d93551b520d2720ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426561c9e76e5e3481e80831fa52bc64",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "e04ba7e2141574618113",
+    "url": "/goit-react-hw-01-components/static/css/main.37db3e3b.chunk.css"
+  },
+  {
+    "revision": "bb3740b2ed64e1b13fec",
+    "url": "/goit-react-hw-01-components/static/js/2.87c7f06b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.87c7f06b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04ba7e2141574618113",
+    "url": "/goit-react-hw-01-components/static/js/main.4239bcb4.chunk.js"
+  },
+  {
+    "revision": "df4b2270b7f8a13b5119",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.438f3515.js"
+  },
+  {
+    "revision": "b4d2c4c39853ee244272c04999b230ba",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-regular.b4d2c4c3.woff2"
+  },
+  {
+    "revision": "b8ee546acd6cc0c49f42ad3d48ef244f",
+    "url": "/goit-react-hw-01-components/static/media/lato-v16-latin-regular.b8ee546a.woff"
+  }
+]);
